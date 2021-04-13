465f3fdd49ae60103a8c46a8cb0473bd5ed08967
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2021-04-12 23:13:37 -0500

    commit tree 15332879a3b7d225e635a1f28bf53c27935b7e9c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(words="roller-skate") {
+    return `This Saturday, I want to ${words}!`;
+
+}
+
+function mondayWork(action="go to the office"){
+    return `This Monday, I will ${action}.`;
+}
+
+function wrapAdjective(sym="*"){
+    const innerFunction = function(condition="special"){
+        return `You are ${sym}${condition}${sym}!`;
+
+    }
+    return innerFunction;
+    
+
+}
